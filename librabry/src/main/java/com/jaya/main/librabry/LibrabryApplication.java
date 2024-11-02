462e324bb42e8d13a99d6b072035bc65853c780e
Krrish <kpgurjar002@gmail.com>
2024-11-02 21:05:29 +0530

    add lib

---
+++ librabry/src/main/java/com/jaya/main/librabry/LibrabryApplication.java
@@ -0,0 +1,13 @@
+package com.jaya.main.librabry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrabryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrabryApplication.class, args);
+	}
+
+}
---
+++ librabry/src/test/java/com/jaya/main/librabry/LibrabryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaya.main.librabry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrabryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
